4a898fbdb9b44439652a353607977bf24a7896bf
Vinit Shah <vinitshah9942@gmail.com>
2020-08-23 12:16:30 -0500

    update at 8/23 at 12:16

--- rocket.py
+++ rocket.py
@@ -19,7 +19,6 @@ class Rocket():
 
 rocket_0 = Rocket(name="Vinit")
 rocket_0.move_rocket(1, 1)
-
 rocket_1 = Rocket(10, 5)
 
 print(rocket_0.name, rocket_0.get_distance(rocket_1), rocket_0.fuel_level)
